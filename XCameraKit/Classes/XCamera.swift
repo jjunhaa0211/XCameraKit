71a9790b26ff7e9ea878a8508e71d4ab11ff21f6
junha <goodjunha@gmail.com>
2023-04-16 14:48:38 +0900

    🎇:: required init fix

--- XCameraKit/Classes/XCamera.swift
+++ XCameraKit/Classes/XCamera.swift
@@ -11,39 +11,42 @@ open class XCamera: UIView {
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        //backgroundColor
+        commonInit()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        // background color
         backgroundColor = .black
-        
+
         // AVCaptureDevice
         let cameraDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back)
-        
+
         cameraDevice = cameraDeviceDiscoverySession.devices.first
-        
+
         // make AVCaptureDeviceInput
         do {
             cameraInput = try AVCaptureDeviceInput(device: cameraDevice)
         } catch let error {
             print("Error creating AVCaptureDeviceInput: \(error.localizedDescription)")
-                        return
+            return
         }
-        
-        //make AVCaptureVideoPreviewLayer
+
+        // make AVCaptureVideoPreviewLayer
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = .resizeAspectFill
         layer.addSublayer(previewLayer)
     }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    open override func layoutSubviews() {
-         super.layoutSubviews()
-        
-        //AVCaptureVideoPreviewLayer frame == XCamera UIview bounds
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        // AVCaptureVideoPreviewLayer frame == XCamera UIview bounds
         previewLayer.frame = bounds
-        
     }
     
     open func startRunning() {
