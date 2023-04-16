414dbabd8d10aaf114063ecd1dac47f7736b7f6e
junha <goodjunha@gmail.com>
2023-04-16 20:05:15 +0900

    🌠:: camera ratio

--- XCameraKit/Classes/XCamera.swift
+++ XCameraKit/Classes/XCamera.swift
@@ -2,15 +2,24 @@ import UIKit
 import Foundation
 import AVFoundation
 
+public enum CameraAspectRatio {
+    case square // 1 : 1
+    case full // UIView bounds
+    case portrait // length 16 : 9
+    case landscape // width 16 : 9
+}
+
 open class XCamera: UIView {
     
     let captureSession = AVCaptureSession()
     var cameraDevice: AVCaptureDevice!
     var cameraInput: AVCaptureDeviceInput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var aspectRatio: CameraAspectRatio = .full
     
     public override init(frame: CGRect) {
         super.init(frame: frame)
+        
         commonInit()
     }
     
@@ -21,7 +30,7 @@ open class XCamera: UIView {
     
     private func commonInit() {
         // background color
-        backgroundColor = .black
+        backgroundColor = .red
 
         // AVCaptureDevice
         let cameraDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back)
@@ -47,13 +56,29 @@ open class XCamera: UIView {
 
         // AVCaptureVideoPreviewLayer frame == XCamera UIview bounds
         previewLayer.frame = bounds
+        
+        let size: CGSize
+        switch aspectRatio {
+        case .square:
+            size = CGSize(width: min(bounds.width, bounds.height), height: min(bounds.width, bounds.height))
+        case .full:
+            size = bounds.size
+        case .portrait:
+            size = CGSize(width: bounds.height * 9 / 16, height: bounds.height)
+        case .landscape:
+            size = CGSize(width: bounds.width, height: bounds.width * 3 / 4)
+        }
+
+        let previewLayerFrame = CGRect(origin: .zero, size: size)
+        previewLayer.frame = previewLayerFrame
+        previewLayer.position = CGPoint(x: bounds.width / 2, y: bounds.maxY / 2)
     }
     
     open func startRunning() {
         //AVCaptureDeviceInput, AVCaptureSessioning != AVCaptureSession start
         if cameraInput != nil && !captureSession.isRunning {
             captureSession.beginConfiguration()
-            captureSession.sessionPreset = .photo
+            captureSession.sessionPreset = .high
             if captureSession.canAddInput(cameraInput) {
                 captureSession.addInput(cameraInput)
             }
@@ -67,5 +92,10 @@ open class XCamera: UIView {
         if captureSession.isRunning {
             captureSession.stopRunning()
         }
+    }
+    
+    open func setAspectRatio(_ aspectRatio: CameraAspectRatio) {
+        self.aspectRatio = aspectRatio
+        setNeedsLayout()
     }
 }
